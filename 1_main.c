12538f68f79ac2eb49e433f148b3783c4c2c5336
aleinl <19150945470@163.com>
2025-10-20 20:57:23 +0800

    Update 1_main.c
    //202510303129
    //19150945470@163.com
    //陶磊
    #include<stdio.h>
     int main()
    {
       float a;
       printf("请输入摄氏度：");
       scanf("%f,",&a);
       float b;
       
       b=a*9/5+32;
       
       printf("华氏摄氏度:%.1f",b);
    
       return 0;
    }

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,17 @@
-//
-//
-//
-#include <stdio.h>
-int main()
+//202510303129
+//19150945470@163.com
+//陶磊
+#include<stdio.h>
+ int main()
 {
-    printf("Hello World");
-    return 0;
+   float a;
+   printf("请输入摄氏度：");
+   scanf("%f,",&a);
+   float b;
+   
+   b=a*9/5+32;
+   
+   printf("华氏摄氏度:%.1f",b);
+
+   return 0;
 }
